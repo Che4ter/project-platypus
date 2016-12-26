f114f3d75ca8eea257e168e7cf960817a1674678
Che4ter <che4ter@42.wtf>
2016-12-26 23:49:35 +0100

    Add User route

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,17 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
+$app->get('/', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+//USER
+$app->get('/user', function ($request, $response, $args) {
+    $data = array('name' => 'Rob', 'age' => 40);
+    return $response->withJson($data, 201);
 });
