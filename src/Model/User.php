511e9374dab10d235856c9c4d9a950f49479d8f2
Che4ter <che4ter@42.wtf>
2016-12-28 20:25:00 +0100

    Enable Eloquent timestamps in user model

--- src/Model/User.php
+++ src/Model/User.php
@@ -5,7 +5,6 @@ namespace Platypus\Model;
 class User extends \Illuminate\Database\Eloquent\Model
 {
     protected $fillable = ['mailaddress'];
-    public $timestamps = false;
 
     /**
      * Get the roles for the user.
