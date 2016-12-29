39314c630f1cda55e90c2f27c3b35d4f26bfc431
Mogria <m0gr14@gmail.com>
2016-12-29 13:41:07 +0100

    implement a few test cases for the UserController

--- tests/Functional/UserControllerTest.php
+++ tests/Functional/UserControllerTest.php
@@ -1,12 +1,47 @@
 <?php
 
 namespace Tests\Functional;
+use Platypus\Model\User;
 
 class UserControllerTest extends BaseTestCase
 {
+
+    public function setUp() {
+        $this->createApp();
+        $this->beginTransaction();
+    }
+
+    public function tearDown() {
+        $this->rollback();
+    }
+
     public function test_UserRequest_returnsAllUsers()
     {
         $response = $this->runApp('GET', '/api/v1/user');
         $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function test_UserRequest_createUser() {
+        $response = $this->runApp('POST', '/api/v1/user', [
+            'mailaddress' => 'test@mail.com',
+            'password' => 'testpw',
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertTrue(User::where('mailaddress', 'test@mail.com')->first() != null);
+    }
+
+    public function test_UserRequest_createUser_alreadyExists() {
+        $responseCreate = $this->runApp('POST', '/api/v1/user', [
+            'mailaddress' => 'test@mail.com',
+            'password' => 'testpw',
+        ]);
+
+        $responseFail = $this->runApp('POST', '/api/v1/user', [
+            'mailaddress' => 'test@mail.com',
+            'password' => 'testpw2',
+        ]);
+
+        $this->assertEquals(409, $responseFail->getStatusCode());
     }
 }
