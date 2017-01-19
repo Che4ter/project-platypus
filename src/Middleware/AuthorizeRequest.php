fea1c20f0bf1c72e54dc6c3cc19e6b3c4984bc45
Mogria <m0gr14@gmail.com>
2017-01-19 23:52:11 +0100

    fix AuthorizeRequest Middleware check jwt auth correctly. Maybe add a seperate Middleware which puts the jwt token into the request attributes instead of having to access the DI container and needing to pass the container to every AuthorizeRequest check

--- src/Middleware/AuthorizeRequest.php
+++ src/Middleware/AuthorizeRequest.php
@@ -10,14 +10,15 @@ class AuthorizeRequest {
 
     public function __construct(ContainerInterface $container, $leastRequiredRole) {
         $this->container = $container;
+        $this->leastRequiredRole = $leastRequiredRole;
         if($leastRequiredRole < Role::ID_GUEST && $leastRequiredRole > Role::ID_ADMIN) {
             throw new InvalidArgumentException("leastRequiredRole must be between Role::ID_GUEST and Role::ID_ADMIN");
         }
     }
 
     public function __invoke($request, $response, $next) {
-        if (!$this->container->has("jwt")
-            || $this->container->get("jwt")->role_id < $this->leastRequiredRole) {
+        if (!isset($this->container["jwt"])
+            || ($this->container->get("jwt")->role_id < $this->leastRequiredRole)) {
             $errorMsg = "You don't have the sufficient role to be able to perform this request.";
             return $response->withJson(["errors" => [$errorMsg]], 401);
         }
